0ff60214c7ced1d04c88936cff77796b7a681e47
Lyndon Michael Bibera <hack.of.all.codes@gmail.com>
2017-02-27 17:08:40 +0800

    added res repo and HAL browser

--- spring-api-server/src/main/java/com/practice/spring/repository/CustomerRepository.java
+++ spring-api-server/src/main/java/com/practice/spring/repository/CustomerRepository.java
@@ -2,12 +2,11 @@ package com.practice.spring.repository;
 
 import com.practice.spring.domain.Customer;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
-/**
- * Created by lbibera on 27/02/2017.
- */
+@RepositoryRestResource
 public interface CustomerRepository extends MongoRepository<Customer, String> {
 
     Customer findByFirstName(String firstName);
