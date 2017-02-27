8bc19d041850e264ccc295a9828d3efd752fc394
Lyndon Michael Bibera <hack.of.all.codes@gmail.com>
2017-02-27 16:40:24 +0800

    generated spring project

---
+++ spring-api-server/src/main/java/com/practice/spring/SpringApiServerApplication.java
@@ -0,0 +1,12 @@
+package com.practice.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiServerApplication.class, args);
+	}
+}
