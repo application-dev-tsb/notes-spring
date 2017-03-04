9e0416b0005c04040922235b73d4f8e07bcf9073
Lyndon Michael Bibera <hack.of.all.codes@gmail.com>
2017-03-04 08:47:49 +0800

    added resource endpoint and frontend communication with the server

--- spring-spa-security/ui/src/main/java/com/practice/spring/spasecurity/ui/UiApplication.java
+++ spring-spa-security/ui/src/main/java/com/practice/spring/spasecurity/ui/UiApplication.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.practice.spring.spasecurity.ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring-spa-security/ui/src/test/java/com/practice/spring/spasecurity/ui/UiApplicationTests.java
+++ spring-spa-security/ui/src/test/java/com/practice/spring/spasecurity/ui/UiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.practice.spring.spasecurity.ui;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
