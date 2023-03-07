bf67c283236e0e18cbe38d993074f710feee59b9
LM Bibera <secret.transaction@gmail.com>
2023-03-07 17:40:00 +0800

    Generated project

---
+++ spring-data-access/src/test/java/dev/codefactory/springdataaccess/SpringDataAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.codefactory.springdataaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
