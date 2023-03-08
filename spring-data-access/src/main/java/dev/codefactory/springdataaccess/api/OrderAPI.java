97cffd8b89eb7dc9c3322f47f980285d713ab533
LM Bibera <secret.transaction@gmail.com>
2023-03-08 14:02:59 +0800

    fix api

--- spring-data-access/src/main/java/dev/codefactory/springdataaccess/api/OrderAPI.java
+++ spring-data-access/src/main/java/dev/codefactory/springdataaccess/api/OrderAPI.java
@@ -4,13 +4,15 @@ import dev.codefactory.springdataaccess.core.entities.Order;
 import dev.codefactory.springdataaccess.core.ports.OrderDatastore;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * we dont want to overcomplicate things since this is not a clean architecture code
  */
 @RequiredArgsConstructor
-@RestController("/orders")
+@RestController
+@RequestMapping("/orders")
 public class OrderAPI {
 
     private final OrderDatastore orderDatastore;
